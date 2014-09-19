9a170688d1e9ae7eed537233250ec7fb10e83d55
Hunter McCurry <hunter@spotify.com>
2014-09-19 11:09:11 -0400

    virtualenv stuff, README, more setup logic

--- setup.sh
+++ setup.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
 dir=~/dotfiles
-olddir=~/dotfiles_old
-files=".gitconfig .zshrc .tmux.conf"
 
 cd $dir
 
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    for file in $files; do
-	rm -f ~/$file
-        ln -s $dir/$file ~/
-    done
+
+# stuff directly in the homedir
+if [[ $REPLY != 'y' ]]; then
+    echo "quitting!"
+    exit
 fi
+
+home_files=".gitconfig .zshrc .tmux.conf"
+for file in $home_files; do
+    rm -f ~/$file
+    ln -s $dir/$file ~/
+    echo $file
+done
 
 rm -f ~/.oh-my-zsh/themes/phuntimes.zsh-theme
 ln -s $dir/phuntimes.zsh-theme ~/.oh-my-zsh/themes/
+
+virtualenv_files="postactivate postdeactivate"
+for file in $virtualenv_files; do
+    rm -f $WORKON_HOME/$file
+    ln -s $dir/virtualenv/$file $WORKON_HOME/$file
+    echo $file
+done
+
